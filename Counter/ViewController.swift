ba7c2569c8713ad482d7112ac5e6829681708f97
kaiot <Psi12@me.com>
2025-08-22 15:09:15 +0300

    добавлены кнопки и история изменений

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -7,13 +7,74 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    
+    var count = 0
+    
+    @IBOutlet weak var textField: UILabel!
+    @IBOutlet weak var counterHistory: UITextView!
+    @IBOutlet weak var counterButton: UIButton!
+    @IBOutlet weak var counterPlusOne: UIButton!
+    @IBOutlet weak var counterMinusOne: UIButton!
+    @IBOutlet weak var reset: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        textField.text = "0"
+        counterHistory.text = "История изменений:\n"
+        counterHistory.isEditable = false
+        counterHistory.isScrollEnabled = true
     }
-
-
+    
+    @IBAction func actionCounterButton(_ sender: Any) {
+        increaseCount()
+    }
+    
+    @IBAction func actionReset(_ sender: Any) {
+        count = 0
+        textField.text = "0"
+        appendHistory("значение сброшено")
+    }
+    
+    
+    @IBAction func actionPlusOne(_ sender: Any) {
+        increaseCount()
+    }
+    
+    
+    @IBAction func actionMinusOne(_ sender: Any) {
+        decreaseCount()
+    }
+    
+    
+    
+    func increaseCount () {
+        count += 1
+        textField.text = String(count)
+        appendHistory("Значение изменено на + 1")
+        
+    }
+    func decreaseCount () {
+        if count > 0 {
+            count -= 1
+            textField.text = String(count)
+            appendHistory("значение изменено на -1")
+        } else {
+            appendHistory("попытка уменьшить значение счётчика ниже 0")
+        }
+    }
+    
+    
+    func appendHistory(_ text: String) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        let timestamp = formatter.string(from: Date())
+        DispatchQueue.main.async {
+            self.counterHistory.text += "\(timestamp): \(text)\n"
+            let range = NSMakeRange(self.counterHistory.text.count - 1, 0)
+            self.counterHistory.scrollRangeToVisible(range)
+        }
+    }
 }
-
